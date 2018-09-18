f5fb94dd5115a651f5f79e6ea3af4a21e3905aba
Davel-11 <davelaltan@gmail.com>
2018-09-18 15:27:25 +0000

    adding additional

--- routes/genres.js
+++ routes/genres.js
@@ -7,7 +7,10 @@ const courses = [
     {id: 1, name: 'course1' },
     {id: 2, name: 'course2' },
     {id: 3, name: 'course3' },
-  {id: 4, name: 'course4' }
+    {id: 4, name: 'course4' }
+    {id: 4, name: 'course5' }
+    {id: 4, name: 'course6' } 
+     
 ]
 
 const Genre = mongoose.model('Genre', new mongoose.Schema({
