ea7da1ea43adc93b8c853c2d2982d281cb00c9f9
JenkinsDatos <44907545+JenkinsDatos@users.noreply.github.com>
2018-11-13 09:03:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
 
-mongoose.connect('mongodb://172.17.0.3/vidly?authSource=admin',{
-  user: 'root',
-  pass: '1234'
-})
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
+//mongoose.connect('mongodb://172.17.0.3/vidly?authSource=admin',{
+ // user: 'root',
+  //pass: '1234'
+//})
+  //.then(() => console.log('Connected to MongoDB...'))
+  //.catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
