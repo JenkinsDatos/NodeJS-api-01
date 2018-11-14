b941253a0ab23338f1e9c6e78d4e64e28aeaafbd
JenkinsDatos <44907545+JenkinsDatos@users.noreply.github.com>
2018-11-14 15:39:49 -0600

    Update genres.js

--- routes/genres.js
+++ routes/genres.js
@@ -4,14 +4,14 @@ const express = require('express');
 const router = express.Router();
 
 const courses = [
-    {id: 1, name: 'course1' },
-    {id: 2, name: 'course2' },
-    {id: 3, name: 'course3' },
-    {id: 4, name: 'course4' },
-    {id: 5, name: 'course5' },
-    {id: 6, name: 'course6' },
-    {id: 7, name: 'course7' },
-    {id: 8, name: 'course8' }
+    {id: 1, name: 'courseA' },
+    {id: 2, name: 'courseB' },
+    {id: 3, name: 'courseC' },
+    {id: 4, name: 'courseD' },
+    {id: 5, name: 'courseE' },
+    {id: 6, name: 'courseF' },
+    {id: 7, name: 'courseG' },
+    {id: 8, name: 'courseH' }
 ]
 
 const Genre = mongoose.model('Genre', new mongoose.Schema({
